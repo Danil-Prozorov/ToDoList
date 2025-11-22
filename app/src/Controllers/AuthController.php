eb8c6ce00b05d19d0493a79c7806b0d72cd77a50
Danil <danil.prozoroff2018@yandex.ru>
2025-11-22 19:03:57 +0300

    Created base for Auth controller and added it in route list

---
+++ app/src/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+class AuthController
+{
+    public function loginView()
+    {
+
+    }
+
+    public function registerView()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
