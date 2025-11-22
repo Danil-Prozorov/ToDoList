9878c02b1a9576c1b59d7d2e7062a6d31a5f5f14
Danil <danil.prozoroff2018@yandex.ru>
2025-11-22 18:08:48 +0300

    Created Middleware interface, Auth middleware and middleware pipline handler

--- app/src/Core/Router/Middleware/AuthMiddleware.php
+++ app/src/Core/Router/Middleware/AuthMiddleware.php
@@ -2,7 +2,30 @@
 
 namespace App\Core\Router\Middleware;
 
-class AuthMiddleware
-{
+use App\Core\Router\Middleware\Interfaces\MiddlewareInterface as Middleware;
+use App\Core\Http\Request;
+use App\Core\Router\Router;
 
+class AuthMiddleware implements Middleware
+{
+    public function handle(Request $request, callable $next)
+    {
+
+        if (!$this->isAuthorized($request)) {
+            (new Router())->redirect("login");
+        }
+
+        return $request;
+    }
+
+    private function isAuthorized(Request $request): bool
+    {
+        $token = $request->getHeaderElement("Cookie");
+
+        if (!str_contains($token, "user_token")) {
+            return false;
+        }
+
+        return true;
+    }
 }
